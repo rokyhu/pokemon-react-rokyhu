53d639cdfd42fd68bd5f7bfac462574bf1b0ff1a
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 18:56:07 +0200

    [add] PropTypes for NavItem

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export default class NavItem extends Component {
     render() {
         return (
-            <a Href={this.props.item.url}><h4 className="NavItem">{this.props.item.title}</h4></a>
+            <a href={this.props.item.url}><h4 className="NavItem">{this.props.item.title}</h4></a>
         )
     }
+}
+
+// PropTypes
+NavItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
