3d87b4b671f6b95f7d4bb09e4810a79107d9c03a
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 22:55:39 +0200

    [add] Type fetch and printing

---
+++ src/components/pages/Type.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+import imgPlaceholder from '../../images/image_placeholder.jpg';
+
+export default class Type extends React.Component {
+
+    state = {
+        typeDetails: null,
+        pokemonDetails: null
+    }
+
+    componentDidMount() {
+        const url = this.props.type.url
+        axios.get(url)
+        .then(res => this.setState({typeDetails: res.data}))
+    }
+
+    getExamplePokemonDetails() {
+        const examplePokemons = this.state.typeDetails.pokemon[0];
+        if (examplePokemons) {
+            const examplePokemonUrl = this.state.typeDetails.pokemon[0].pokemon.url
+            axios.get(examplePokemonUrl)
+            .then(res => this.setState({pokemonDetails: res.data}))
+        }
+    
+    }
+    
+    render() {
+        if (!this.state.typeDetails) {
+            return <div className="Card">
+                <img
+                    src={imgPlaceholder}
+                    alt='Loading...'
+                    width="96"
+                    height="96">
+                </img>
+                <h6>Loading...</h6>
+            </div>
+        }
+
+        this.getExamplePokemonDetails();
+
+        return (
+            <div className="Card">
+                <img
+                    src={!this.state.pokemonDetails ? imgPlaceholder : this.state.pokemonDetails.sprites.front_default}
+                    // src={imgPlaceholder}
+                    alt={this.state.typeDetails.name}
+                    width="96"
+                    height="96">
+                </img>
+                <h6>{this.state.typeDetails.name}</h6>
+            </div>
+        )
+    }
+}
