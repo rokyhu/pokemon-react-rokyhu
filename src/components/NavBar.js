b98383e110f61e29ebd675614d498d5d3f14bb09
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 18:56:02 +0200

    [add] PropTypes for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
-import NavItem from './NavItem'
+import React, { Component } from 'react';
+import NavItem from './NavItem';
+import PropTypes from 'prop-types';
 
 export default class NavBar extends Component {
 
     render() {
         return this.props.menuItems.map((item) => (
-            <NavItem item={item} />
+            <NavItem key={item.id} item={item} />
         ));
     }
+}
+
+// PropTypes
+NavBar.propTypes = {
+    menuItems: PropTypes.array.isRequired    
 }
