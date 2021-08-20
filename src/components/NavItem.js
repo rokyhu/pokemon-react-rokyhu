7fabda84e7aa7efb2121ba243bc1f6627c16ca00
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 14:55:54 +0200

    [add] styling to NavBar

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class NavItem extends Component {
     render() {
         return (
-            <a Href={this.props.item.url}><h6 className="NavItem">{this.props.item.title}</h6></a>
+            <a Href={this.props.item.url}><h4 className="NavItem">{this.props.item.title}</h4></a>
         )
     }
 }
