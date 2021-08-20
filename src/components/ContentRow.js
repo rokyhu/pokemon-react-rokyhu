a82e7586dc8506a84cf5ac897abb0487cc68b362
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 18:57:26 +0200

    [add] empty ContentRow

---
+++ src/components/ContentRow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContentRow extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
