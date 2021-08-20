d2be183bb328299f097fbf1301a6ade9f62966c7
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-20 14:43:28 +0200

    [refactor] PageHeader to class, add menuItems state

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import NavBar from './NavBar'
 
-export default function PageHeader() {
-    return (
-        <div className="App-header">
-            <h3>Helló</h3>
-        </div>
-    )
+export default class PageHeader extends React.Component {
+
+    state = {
+        menuItems: [
+            {
+                id: 1,
+                title: "Pokemons",
+                url: "./pokemons"
+            },
+            {
+                id: 2,
+                title: "Types",
+                url: "./types"
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App-header">
+                <NavBar menuItems={this.state.menuItems}/>
+            </div>
+        )
+    }
 }
