05e638b364f898093f83a0d2baa7d48bd673b996
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-22 23:32:52 +0200

    [update] Footer formatting

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PageFooter() {
     return (
         <div className="App-footer">
-            <p>© 2021 Zsolt 'Roky' Rokolya</p>
+            <p>© 2021 Zsolt 'Roky' Rokolya | Codecool | Advanced Java React Pet Project</p>
         </div>
     )
 }
