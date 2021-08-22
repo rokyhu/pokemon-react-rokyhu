5122666002b41d784cf4004e1171619eb189da19
Zsolt Rokolya <rokyhu@gmail.com>
2021-08-22 23:48:30 +0200

    [refactor] TypeCard, add styled components

--- src/components/pages/TypeCard.js
+++ src/components/pages/TypeCard.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 import imgPlaceholder from '../../images/image_placeholder.jpg';
 
-export default function Type(props) {
+export default function TypeCard(props) {
 
     const [typeDetails, setTypeDetails] = useState();
     const [pokemonDetails, setPokemonDetails] = useState();
@@ -12,6 +13,24 @@ export default function Type(props) {
         axios.get(url)
         .then(res => setTypeDetails(res.data))
     }, [props]);
+
+    const Card = styled.button`
+        width: 240px;
+        height: 96px;
+        background-color: white;
+        margin: 10px;
+        padding: 10px;
+        border-radius: 10px;
+        border: none;
+        display: flex;
+        align-items: center;
+        box-shadow: 3px 3px #eee;
+        text-transform: capitalize;
+        cursor: pointer;
+        &:hover {
+            box-shadow: 3px 3px #aaa;
+        }
+    `
 
     const getExamplePokemonDetails = () => {
         const examplePokemons = typeDetails.pokemon[0];
@@ -27,7 +46,7 @@ export default function Type(props) {
     }
 
     return (
-        <div className="Card">
+        <Card>
             <img
                 src={!pokemonDetails ? imgPlaceholder : pokemonDetails.sprites.front_default}
                 alt={!pokemonDetails ? 'Loading...' : typeDetails.name}
@@ -35,6 +54,6 @@ export default function Type(props) {
                 height="96">
             </img>
             <p>{!pokemonDetails ? 'Loading...' : typeDetails.name}</p>
-        </div>
+        </Card>
     )
 }
